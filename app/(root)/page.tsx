39ffbf7a171658db7fa3ed8990e7decb5edbffb5
omid <itmanager.abbasi@gmail.com>
2024-07-22 14:53:05 +0330

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>page</section>
+  )
+}
+
+export default page
