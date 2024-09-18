d1d5f28e2175fd1cfe9c0e5e1ee383c920dd855b
omid <itmanager.abbasi@gmail.com>
2024-09-18 15:07:58 +0330

    order actions

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -4,40 +4,40 @@ import Stripe from 'stripe';
 import { CheckoutOrderParams } from "@/types";
 import { redirect } from 'next/navigation';
 
-export const checkoutOrder=async(order:CheckoutOrderParams)=>{
-    const stripe=new Stripe(process.env.STRIPE_SECRET_KEY!);
+export const checkoutOrder = async (order: CheckoutOrderParams) => {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-      if (!stripe) {
+    if (!stripe) {
         return;
-      }
+    }
 
-      const price=Number(order.price) * 100;
+    const price = Number(order.price) * 100;
 
-      try {
-        const session=await stripe.checkout.sessions.create({
-            line_items:[
+    try {
+        const session = await stripe.checkout.sessions.create({
+            line_items: [
                 {
-                    price_data:{
-                        currency:"usd",
-                        unit_amount:price,
-                        product_data:{
-                            name:order.productTitle
+                    price_data: {
+                        currency: "usd",
+                        unit_amount: price,
+                        product_data: {
+                            name: order.productTitle
                         }
                     },
-                    quantity:1
+                    quantity: 1
                 }
             ],
-            metadata:{
-                productId:order.productId,
-                buyerId:order.buyerId
+            metadata: {
+                productId: order.productId,
+                buyerId: order.buyerId
             },
-            mode:'payment',
-            success_url:`${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-            cancel_url:`${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+            mode: 'payment',
+            success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+            cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
         });
 
         redirect(session.url!);
-      } catch (error) {
-        
-      }
+    } catch (error) {
+        throw error;
+    }
 }
